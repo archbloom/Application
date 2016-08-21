033d9091f0d3f7ee44383daff32539dc0d3e8a2f
Bhushan Authankar <bhushanauthankar@gmail.com>
2016-08-21 23:09:37 +0530

    Add SPA boilerplate

--- components/Home.jsx
+++ components/Home.jsx
@@ -19,11 +19,6 @@ function Home() {
         <a href={ghPagesLink}>GitHub&nbsp;Pages</a> incompatibility
         with single page apps (like this one).
       </p>
-      <p>
-        Please see the <a href={repoReadmeLink}>repo readme</a> for
-        instructions on how to use this boilerplate
-        to deploy your own single page app using GitHub Pages.
-      </p>
       <div><Link to="/example">Example page</Link></div>
       <div><Link to="/example/two-deep?field1=foo&field2=bar#boom!">
         Example two deep with query and hash
